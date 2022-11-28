3bc5857fbdc27520f350173a0a77e95703757fd2
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-11-28 15:59:15 -0800

    getById returns a restaurant with nested reviews

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -3,7 +3,6 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
-const authenticate = require('../lib/middleware/authenticate');
 
 // Dummy user for testing
 const mockUser = {
@@ -41,6 +40,68 @@ describe('restaurant routes', () => {
     const [agent] = await registerAndLogin();
     const res = await agent.get('/api/v1/restaurants');
     expect(res.status).toEqual(200);
+    expect(res.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "cost": 1,
+          "cuisine": "American",
+          "id": "1",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
+          "name": "Pip's Original",
+          "website": "http://www.PipsOriginal.com",
+        },
+        Object {
+          "cost": 3,
+          "cuisine": "Italian",
+          "id": "2",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/13/af/df/89/duck.jpg",
+          "name": "Mucca Osteria",
+          "website": "http://www.muccaosteria.com",
+        },
+        Object {
+          "cost": 2,
+          "cuisine": "Mediterranean",
+          "id": "3",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/1c/f2/e5/0c/dinner.jpg",
+          "name": "Mediterranean Exploration Company",
+          "website": "http://www.mediterraneanexplorationcompany.com/",
+        },
+        Object {
+          "cost": 2,
+          "cuisine": "American",
+          "id": "4",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-o/0d/d6/a1/06/chocolate-gooey-brownie.jpg",
+          "name": "Salt & Straw",
+          "website": "https://saltandstraw.com/pages/nw-23",
+        },
+      ]
+    `);
   });
-  
+
+  it('GET api/v1/restaurants/:id returns details and nested reviews of a restaurant', async () => {
+    const [agent] = await registerAndLogin();
+    const res = await agent.get('/api/v1/restaurants/1');
+    expect(res.status).toEqual(200);
+    expect(res.body).toMatchInlineSnapshot(`
+      Object {
+        "cost": 1,
+        "cuisine": "American",
+        "id": "1",
+        "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
+        "name": "Pip's Original",
+        "reviews": Array [
+          Object {
+            "id": "1",
+          },
+          Object {
+            "id": "2",
+          },
+          Object {
+            "id": "3",
+          },
+        ],
+        "website": "http://www.PipsOriginal.com",
+      }
+    `);
+  });
 });
